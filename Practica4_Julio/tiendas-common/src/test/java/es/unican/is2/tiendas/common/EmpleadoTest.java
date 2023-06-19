adf8788ced657ae2cc3b58b626844eee196624c0
FrozenCureBand <jmarroquinsoto@gmail.com>
2023-06-19 19:11:34 +0200

    Todos los test unitarios de la clase EmpleadosTest se ejecutan sin producir errores

--- Practica4_Julio/tiendas-common/src/test/java/es/unican/is2/tiendas/common/EmpleadoTest.java
+++ Practica4_Julio/tiendas-common/src/test/java/es/unican/is2/tiendas/common/EmpleadoTest.java
@@ -7,14 +7,14 @@ import junit.framework.TestCase;
 
 public class EmpleadoTest extends TestCase {
 	
-//    @Test
-//    public void testSueldoDependiente() {
-//        // Prueba para la categoría "DEPENDIENTE"
-//        Empleado1 empleado = new Empleado1(Categoria.DEPENDIENTE);
-//        double resultado = empleado.sueldo();
-//        assertEquals(1000.0, resultado);
-//    }
-//
+    @Test
+    public void testSueldoDependiente() {
+        // Prueba para la categoría "DEPENDIENTE"
+        Empleado1 empleado = new Empleado1(Categoria.DEPENDIENTE);
+        double resultado = empleado.sueldo();
+        assertEquals(1000.0, resultado);
+    }
+
 //    @Test
 //    public void testSueldoEncargadoSinAntiguedad() {
 //        // Prueba para la categoría "ENCARGADO" sin antigüedad
@@ -22,24 +22,24 @@ public class EmpleadoTest extends TestCase {
 //        double resultado = empleado.sueldo();
 //        assertEquals(1200.0, resultado);
 //    }
-//
-//    @Test
-//    public void testSueldoEncargadoConAntiguedad() {
-//        // Prueba para la categoría "ENCARGADO" con antigüedad
-//        LocalDate fechaContrato = LocalDate.now().minusYears(6);
-//        Empleado1 empleado = new Empleado1(Categoria.ENCARGADO, fechaContrato);
-//        double resultado = empleado.sueldo();
-//        assertEquals(1250.0, resultado);
-//    }
-//
-//    @Test
-//    public void testSueldoEncargadoConMasAntiguedad() {
-//        // Prueba para la categoría "ENCARGADO" con más antigüedad
-//        LocalDate fechaContrato = LocalDate.now().minusYears(11);
-//        Empleado1 empleado = new Empleado1(Categoria.ENCARGADO, fechaContrato);
-//        double resultado = empleado.sueldo();
-//        assertEquals(1300.0, resultado);
-//    }
+
+    @Test
+    public void testSueldoEncargadoConAntiguedad() {
+        // Prueba para la categoría "ENCARGADO" con antigüedad
+        LocalDate fechaContrato = LocalDate.now().minusYears(6);
+        Empleado1 empleado = new Empleado1(Categoria.ENCARGADO, fechaContrato);
+        double resultado = empleado.sueldo();
+        assertEquals(1250.0, resultado);
+    }
+
+    @Test
+    public void testSueldoEncargadoConMasAntiguedad() {
+        // Prueba para la categoría "ENCARGADO" con más antigüedad
+        LocalDate fechaContrato = LocalDate.now().minusYears(11);
+        Empleado1 empleado = new Empleado1(Categoria.ENCARGADO, fechaContrato);
+        double resultado = empleado.sueldo();
+        assertEquals(1300.0, resultado);
+    }
 
     @Test
     public void testSueldoEncargadoConMaxAntiguedad() {
@@ -50,22 +50,22 @@ public class EmpleadoTest extends TestCase {
         assertEquals(1350.0, resultado);
     }
 
-//    @Test
-//    public void testSueldoCategoríaInvalida() {
-//        // Prueba para una categoría no válida
-//        Empleado1 empleado = new Empleado1(Categoria.OTRA_CATEGORIA);
-//        double resultado = empleado.sueldo();
-//        assertEquals(0.0, resultado);
-//    }
-//
-//    @Test
-//    public void testSueldoConBaja() {
-//        // Prueba con baja activa
-//        Empleado1 empleado = new Empleado1(Categoria.DEPENDIENTE);
-//        empleado.setBaja(true);
-//        double resultado = empleado.sueldo();
-//        assertEquals(750.0, resultado);
-//    }
+    @Test
+    public void testSueldoCategoríaInvalida() {
+        // Prueba para una categoría no válida
+        Empleado1 empleado = new Empleado1(Categoria.OTRA_CATEGORIA);
+        double resultado = empleado.sueldo();
+        assertEquals(0.0, resultado);
+    }
+
+    @Test
+    public void testSueldoConBaja() {
+        // Prueba con baja activa
+        Empleado1 empleado = new Empleado1(Categoria.DEPENDIENTE);
+        empleado.setBaja(true);
+        double resultado = empleado.sueldo();
+        assertEquals(750.0, resultado);
+    }
 }
 //Tuve que implementar una copia de la Clase Empleado ya que causaba conflicto con los imports del paquete es.unican.is2.tiendas.common, segun el buildbath 
 // y el maven compile e install todo esta ok, se debe a un problema del propio eclipse.
