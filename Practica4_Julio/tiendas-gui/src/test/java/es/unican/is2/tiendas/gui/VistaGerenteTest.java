20e40e68baff5d69fed75e1c0503fe48f3cf6c68
FrozenCureBand <jmarroquinsoto@gmail.com>
2023-06-20 22:35:38 +0200

    Creacion clase Test de la clase VistaGerente de la capa de presentación, en el pom ya se añadio la dependencia para FEST

---
+++ Practica4_Julio/tiendas-gui/src/test/java/es/unican/is2/tiendas/gui/VistaGerenteTest.java
@@ -0,0 +1,14 @@
+package es.unican.is2.tiendas.gui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VistaGerenteTest {
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
