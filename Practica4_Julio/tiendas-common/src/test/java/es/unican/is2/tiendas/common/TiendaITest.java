f2b9de238e9f035ba2350a635704a7af958523f7
FrozenCureBand <jmarroquinsoto@gmail.com>
2023-06-21 15:43:57 +0200

    Ultimos cambios a las clases unitarias y de integracion

--- Practica4_Julio/tiendas-common/src/test/java/es/unican/is2/tiendas/common/TiendaITest.java
+++ Practica4_Julio/tiendas-common/src/test/java/es/unican/is2/tiendas/common/TiendaITest.java
@@ -3,7 +3,10 @@ package es.unican.is2.tiendas.common;
 import es.unican.is2.tiendas.business.GestionTiendas;
 import es.unican.is2.tiendas.dao.*;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class TiendaITest {
 
@@ -24,18 +27,37 @@ public class TiendaITest {
         // Verificar el resultado esperado
         assertEquals(3050.0, gastoMensual);
     }
-
-//    @Test
-//    public void testGetEmpleados() {
-//        // Obtener la lista de empleados de la tienda
-//        List<Empleado> empleadosObtenidos = tienda.getEmpleados();
-//
-//        // Verificar que la lista de empleados es la misma que la configurada anteriormente
-//        assertEquals(2, empleadosObtenidos.size());
-//        assertEquals(empleado1, empleadosObtenidos.get(0));
-//        assertEquals(empleado2, empleadosObtenidos.get(1));
-//    }
     
+    @Test
+    public void testGastoMensualSueldos2() {
 
+    	GestionTiendas gt = new GestionTiendas(tiendasDAO, vehiculosDAO);
+    	Tienda t1 = new Tienda();
+    	t1 = gt.tienda("MaliañoCentro");
+        double gastoMensual = t1.gastoMensualSueldos();
+
+        // Verificar el resultado esperado
+        assertEquals(2300.0, gastoMensual);
+    }
+    
+    @Test
+    public void testGastoMensualSueldosTiendaInexistente() {
+        // Verificar el resultado esperado
+        try {
+			for (int i = 0; i < 11; i++) {
+				GestionTiendas gt = new GestionTiendas(tiendasDAO, vehiculosDAO);
+		    	Tienda t1 = new Tienda();
+		    	t1 = gt.tienda("BarakaldoCentro");
+		        double gastoMensual = t1.gastoMensualSueldos();
+			}
+	         fail("Tienda Inxistente en DAO");
+	     } catch (final RuntimeException e) {
+	         assertTrue(true);
+	     }
+    }
+    
 }
+            	
 
+
+	     
